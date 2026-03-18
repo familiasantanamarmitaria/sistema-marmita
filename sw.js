c8930cad79a2f8a88839ce9ba2466fd88648c4f0
familiasantanamarmitaria <familiasantanamarmitaria@gmail.com>
2026-03-17 23:22:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,14 @@
-const CACHE_NAME = 'marmitaria-v7.2-cache';
+const CACHE_NAME = 'santana-v7-cloud';
 const assets = [
   './',
   './index.html',
-  'https://assets.mixkit.co/active_storage/sfx/2013/2013-preview.mp3'
+  './manifest.json'
 ];
 
-// Instalação: Salva os arquivos essenciais
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Estratégia: Network First, falling back to cache
-// Tentamos baixar o código mais novo, se falhar (offline), usa o cache.
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
